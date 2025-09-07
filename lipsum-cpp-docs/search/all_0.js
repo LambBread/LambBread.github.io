050d29b11aac266805b9e4f5fe6b9564a47c348b
LambBread <139511212+LambBread@users.noreply.github.com>
2025-09-07 11:56:28 -0700

    Add files via upload

---
+++ lipsum-cpp-docs/search/all_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['generatedefaultlipsumsentence_0',['GenerateDefaultLipsumSentence',['../lipsum_8hpp.html#acb9f05ef38b7b4e53fba81b239459ddb',1,'lipsum']]],
+  ['generateparagraph_1',['GenerateParagraph',['../lipsum_8hpp.html#a40bd5f25ea2983a96c6b70607a5dfeeb',1,'lipsum']]],
+  ['generateparagraphs_2',['GenerateParagraphs',['../lipsum_8hpp.html#a0a547cf8c79563ab4ce12d20e546b93d',1,'lipsum']]],
+  ['generatesentence_3',['GenerateSentence',['../lipsum_8hpp.html#ac588bd5262568cd620cc69131a4d1bd1',1,'lipsum']]],
+  ['generatesentences_4',['GenerateSentences',['../lipsum_8hpp.html#a70dd1edd162a6065f4d0b1cf1dc19f2f',1,'lipsum']]],
+  ['generateword_5',['GenerateWord',['../lipsum_8hpp.html#a65e2d9988838f2f1058d0dcac9edf2fa',1,'lipsum']]]
+];
---
+++ lipsum-cpp-docs/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['lipsum_2eh_0',['lipsum.h',['../lipsum_8h.html',1,'']]],
+  ['lipsum_2ehpp_1',['lipsum.hpp',['../lipsum_8hpp.html',1,'']]]
+];
