15711d1938796529625981ced27bbe881da65387
LambBread <judahjohnson4702@gmail.com>
2025-12-04 17:47:40 -0800

    lipsum-cpp version 0.2.2

--- lipsum-cpp-docs/search/searchdata.js
+++ lipsum-cpp-docs/search/searchdata.js
@@ -5,7 +5,8 @@ var indexSectionsWithContent =
   2: "l",
   3: "l",
   4: "aghlprsw",
-  5: "m"
+  5: "m",
+  6: "l"
 };
 
 var indexSectionNames =
@@ -15,7 +16,8 @@ var indexSectionNames =
   2: "namespaces",
   3: "files",
   4: "functions",
-  5: "variables"
+  5: "variables",
+  6: "defines"
 };
 
 var indexSectionLabels =
@@ -25,6 +27,7 @@ var indexSectionLabels =
   2: "Namespaces",
   3: "Files",
   4: "Functions",
-  5: "Variables"
+  5: "Variables",
+  6: "Macros"
 };
 
